026449087eb1b4ad4487ef55a7c870cc021334e9
martintrifunov <martintrifunov@proton.me>
2025-11-29 21:54:29 +0100

    Modify problems controller to includen new problem state

--- app/controllers/problems_controller.ts
+++ app/controllers/problems_controller.ts
@@ -2,9 +2,11 @@ import type { HttpContext } from '@adonisjs/core/http'
 import { DateTime } from 'luxon'
 import User from '#models/user'
 import Problem from '#models/problem'
+import UserProblemState from '#models/user_problem_state'
+import { ProblemGeneratorService } from '#services/problem_generator_service'
 
 export default class ProblemsController {
-  
+
   async getProblem({ request, response }: HttpContext) {
     const { discord_id, username } = request.body()
 
@@ -12,39 +14,52 @@ export default class ProblemsController {
       return response.badRequest({ error: 'Missing discord_id or username' })
     }
 
-    await User.updateOrCreate(
+    const user = await User.updateOrCreate(
       { discordId: discord_id },
       { username: username }
     )
 
-    const now = DateTime.fromISO('2025-12-01') 
+    const now = DateTime.fromISO('2025-12-05') 
     // const now = DateTime.now()
 
     if (now.month !== 12) {
-      return response.ok({ 
-        message: 'There are no FINKI AoC problems at this time. See you in December!' 
-      })
+       return response.ok({ message: 'Not December yet.' })
     }
 
     const day = now.day
     let currentWeek = 0
-
     if (day >= 1 && day <= 7) currentWeek = 1
     else if (day >= 8 && day <= 14) currentWeek = 2
     else if (day >= 15 && day <= 21) currentWeek = 3
     else currentWeek = 4
 
     const problem = await Problem.findBy('week', currentWeek)
+    if (!problem) return response.notFound({ error: 'Problem not found' })
 
-    if (!problem) {
-      return response.notFound({ error: 'Problem for this week not found.' })
+
+    let state = await UserProblemState.query()
+      .where('user_id', user.id)
+      .andWhere('problem_id', problem.id)
+      .first()
+
+    if (!state) {
+      const { input, solution } = ProblemGeneratorService.generate(currentWeek)
+      
+      state = await UserProblemState.create({
+        userId: user.id,
+        problemId: problem.id,
+        inputData: input,
+        solution: solution,
+        isSolved: false
+      })
     }
 
     return response.ok({
       week: currentWeek,
       title: problem.title,
       description: problem.description,
-      input_example: problem.inputExample
+      user_input: state.inputData, 
+      is_solved: state.isSolved
     })
   }
 }
