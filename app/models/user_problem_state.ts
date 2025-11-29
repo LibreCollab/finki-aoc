ceb453c1329da0397b8478e0ee43398a6eaab620
martintrifunov <martintrifunov@proton.me>
2025-11-29 21:53:51 +0100

    Create problem state model

---
+++ app/models/user_problem_state.ts
@@ -0,0 +1,37 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import User from '#models/user'
+import Problem from '#models/problem'
+
+export default class UserProblemState extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare userId: number
+
+  @column()
+  declare problemId: number
+
+  @column()
+  declare inputData: string
+
+  @column()
+  declare solution: string
+
+  @column()
+  declare isSolved: boolean
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+
+  @belongsTo(() => Problem)
+  declare problem: BelongsTo<typeof Problem>
+}
---
+++ database/migrations/1764449260818_create_user_problem_states_table.ts
@@ -0,0 +1,27 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'user_problem_states'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      
+      table.integer('user_id').unsigned().references('users.id').onDelete('CASCADE')
+      table.integer('problem_id').unsigned().references('problems.id').onDelete('CASCADE')
+      
+      table.text('input_data').notNullable()
+      table.string('solution').notNullable()
+      table.boolean('is_solved').defaultTo(false)
+      
+      table.unique(['user_id', 'problem_id'])
+
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
