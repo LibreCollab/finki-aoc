d3cbf68cd45f21b0df36485cfd888ee428e44f68
martintrifunov <martintrifunov@proton.me>
2025-11-30 02:44:23 +0100

    Implement rate limiter

---
+++ start/limiter.ts
@@ -0,0 +1,19 @@
+import limiter from '@adonisjs/limiter/services/main'
+
+export const globalLimiter = limiter.define('global', (ctx) => {
+  return limiter
+    .allowRequests(200)
+    .every('1 minute')
+    .usingKey(`global_${ctx.request.ip()}`)
+})
+
+export const gameLimiter = limiter.define('game', (ctx) => {
+  const discordId = ctx.request.input('discord_id')
+
+  const key = discordId ? `game_user_${discordId}` : `game_ip_${ctx.request.ip()}`
+
+  return limiter
+    .allowRequests(10)
+    .every('1 minute')
+    .usingKey(key)
+})
--- start/routes.ts
+++ start/routes.ts
@@ -1,9 +1,17 @@
 import ProblemsController from '#controllers/problems_controller'
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
+import { gameLimiter, globalLimiter } from './limiter.js'
 
 router.group(() => {
-  router.post('/problem', [ProblemsController, 'getProblem'])
-  router.post('/submit', [ProblemsController, 'submitAnswer'])
+  router.group(() => {
+    router.post('/problem', [ProblemsController, 'getProblem'])
+    router.post('/submit', [ProblemsController, 'submitAnswer'])
+  }).middleware([
+    middleware.botAuth(),
+    gameLimiter
+  ])
+
   router.get('/leaderboard', [ProblemsController, 'getLeaderboard'])
-}).use(middleware.botAuth())
+    .middleware(middleware.botAuth())
+}).use(globalLimiter)
