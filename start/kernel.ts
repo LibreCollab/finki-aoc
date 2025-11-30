a3e3db3359de3c90b1dcf0bc7ca04f0bb6a67e12
martintrifunov <martintrifunov@proton.me>
2025-11-30 01:00:17 +0100

    Remove auth from AdonisJS

--- start/kernel.ts
+++ start/kernel.ts
@@ -32,7 +32,9 @@ server.use([
  * The router middleware stack runs middleware on all the HTTP
  * requests with a registered route.
  */
-router.use([() => import('@adonisjs/core/bodyparser_middleware'), () => import('@adonisjs/auth/initialize_auth_middleware')])
+router.use([
+  () => import('@adonisjs/core/bodyparser_middleware'), 
+])
 
 /**
  * Named middleware collection must be explicitly assigned to
@@ -40,5 +42,4 @@ router.use([() => import('@adonisjs/core/bodyparser_middleware'), () => import('
  */
 export const middleware = router.named({
   botAuth: () => import('#middleware/bot_auth_middleware'),
-  auth: () => import('#middleware/auth_middleware')
 })
